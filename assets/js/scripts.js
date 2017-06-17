fa4c3ab95d4ecd807191230ae545d9b370838abb
Balasubramanian Bhuvaneswaran <bhuvaneswaran.b@danfoss.com>
2017-06-17 19:29:19 +0530

    upload

---
+++ assets/js/scripts.js
@@ -0,0 +1,168 @@
+$chennai_api_bus_routenumbers = "http://52.36.211.72:5555/gateway/Chennai/v1/bus/routenumbers";
+$chennai_api_train_routes = "http://52.36.211.72:5555/gateway/Chennai/v1/train/time";
+$zomato_locations = "http://52.36.211.72:5555/gateway/Zomato/1.0/locations";
+$zomato_geo_code = "http://52.36.211.72:5555/gateway/Zomato/1.0/geocode";
+$flights = "http://52.36.211.72:5555/gateway/Flights%20API/1.0/data/routes.json";
+$airlines = "http://52.36.211.72:5555/gateway/Flights%20API/1.0/data/airlines.json";
+
+$cities = {
+"Ahmedabad" : "AMD",
+"Bengaluru" : "BLR",
+"Chandigarh" : "IXC",
+"Chennai" : "MAA",
+"Coimbatore" : "CJB",
+"Delhi" : "DEL",
+"Hyderabad" : "HYD",
+"Jaipur" : "JAI",
+"Kochi" : "COK",
+"Kolkata" : "CCU",
+"Mumbai" : "BOM",
+"Patna" : "PAT",
+"Pune" : "PNQ",
+"Port Blair" : "IXZ",
+"Surat" : "STV",
+"Srinagar" : "SXR",
+"Thiruvananthapuram" : "TRV",
+"Tirupathi" : "TIR",
+"Varanasi" : "VNS",
+"Vishakapatnam" : "VTZ"
+};
+
+$("document").ready(function(){
+    
+  $("#chennai_api_route").click(function(e){
+    e.preventDefault();
+    $("#loader").show();
+    $(".display-info-right").show();
+    $(".info-div").hide();
+    var planes = [];
+    var fmap = {};
+
+    $src= "https://www.google.com/maps/embed/v1/directions?origin="+$("#source").val()+"&destination="+$("#destination").val()+"&key=AIzaSyCNUQ6YZcl0SvWS0_j1vE94_s5MXwhHY3o";
+    $("#google-map").attr("src", $src);
+
+    setInterval(function(){                        
+      $("#loader").hide();  
+       $(".display-info-right").show();
+       $(".info-div").show(); }, 2000);
+
+    //Bus
+    $.ajax({ 
+      type: 'GET',
+      headers: {"x-Gateway-APIKey": "5f01d036-32c5-4e14-adbd-ac140a6be03f", "Access-Control-Allow-Origin": 'http://localhost'},
+      'Content-Type' : 'application/x-www-form-urlencoded; charset=UTF-8',
+      url: $chennai_api_bus_routenumbers + "?source="+$("#source").val()+"&destination="+$("#destination").val(),          
+      success: function (data) {
+      
+          if(data == ''){
+            $("#bus_route").html("No bus available");
+          } else {
+              $(data).each(function(i, item){ 
+                  $("#bus_route").html(item.route_Num);
+              })
+          }
+        },
+      error: function (data) {  
+          $("#loader").hide(); 
+          $(".info-div").show(); 
+          $(".display-info-right").show();
+          $("#bus_route").html("No bus available");
+        },
+      });
+
+    //Train
+    $.ajax({ 
+      type: 'GET',
+      headers: {"x-Gateway-APIKey": "5f01d036-32c5-4e14-adbd-ac140a6be03f", "Access-Control-Allow-Origin": 'http://localhost'},
+      'Content-Type' : 'application/x-www-form-urlencoded; charset=UTF-8',
+      url: $chennai_api_train_routes + "?source="+$("#source").val()+"&destination="+$("#destination").val(),          
+      success: function (data) {
+          if(data == ''){
+            $("#train_time_route").html("No train available");            
+          } else {
+              $(data).each(function(i, item){     
+                  $train = item.time +", "+item.type;                  
+                  $("#train_time_route").html( $train);
+              })
+          }
+        },
+      error: function (data) {  
+          $("#train_route").html("No train available");
+        },
+      });
+
+    //Zomato location
+    $.ajax({ 
+      type: 'GET',
+      headers: {"x-Gateway-APIKey": "e127e49c-ef9d-48ba-a8bc-7e815bdc5e3b", "Access-Control-Allow-Origin": 'http://localhost'},
+      'Content-Type' : 'application/x-www-form-urlencoded; charset=UTF-8',
+      url: $zomato_locations + "?&query="+$("#destination").val(),          
+      success: function (data) {       
+          $(data.location_suggestions).each(function(i, item){              
+              $latitude = item.latitude;
+              $longitude = item.longitude;
+          });
+
+              //Zomato zeo code
+                $.ajax({ 
+                  type: 'GET',
+                  headers: {"x-Gateway-APIKey": "e127e49c-ef9d-48ba-a8bc-7e815bdc5e3b", "Access-Control-Allow-Origin": 'http://localhost'},
+                  'Content-Type' : 'application/x-www-form-urlencoded; charset=UTF-8',
+                  url: $zomato_geo_code + "?&lat="+$latitude+"&lon="+$longitude,          
+                  success: function (data) { 
+                    $content = "";      
+                      $(data.nearby_restaurants).each(function(i, item){              
+                         if(item.restaurant.featured_image != ''){
+                            $content += "<div class='row-info-panel row'><div class='col-xs-2 nopadding'><img src="+item.restaurant.featured_image+" style='width:100px;height:100px'></div>";
+                            $content += "<div class='col-xs-10 nopadding'><a target='_blank' href="+item.restaurant.events_url+">"+item.restaurant.name+"</a></br>";
+                            $content += "<div>"+item.restaurant.location.address+"</div></div></div>";
+                         }  
+                      });
+                      $(".restaurants-body").append($content);
+
+                    },
+                  error: function (data) {            
+                      $("#train_route").html("No restaurant");
+                    },
+                });
+
+        },
+      error: function (data) {            
+          $("#train_route").html("No train available");
+        },
+    });    
+
+
+    //Flights location
+    $.ajax({ 
+      type: 'GET',
+      headers: {"x-Gateway-APIKey": "d0536187-7ac8-4a2e-a3d2-52033ceaa077", "Access-Control-Allow-Origin": 'http://localhost'},
+      'Content-Type' : 'application/x-www-form-urlencoded; charset=UTF-8',
+      url: $flights,          
+      success: function (data) {  
+      console.log(data);
+          $(data).each(function(i, item){              
+              $depart = item.departure_airport_iata;
+              $arrival = item.arrival_airport_iata;
+
+                    if($depart == 'MAA' && $arrival == 'DEL'){
+                    var fl = item.airline_iata;
+                    var fno = [];
+                     planes.push(fl);
+                    $(item.planes).each(function(i, plane){                    
+                      //planes.push(fl+'-'+plane);
+                        fno.push(plane);
+                    });
+                    fmap[fl] = fno;
+                  }
+                
+              else {
+                 $(".flights-body").append("No Flights available");
+              }
+          });
+          console.log(planes);
+          }
+    });  
+
+  });
+});
---
+++ index.php
@@ -0,0 +1,102 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta http-equiv="content-type" content="text/html; charset=UTF-8">
+		<meta charset="utf-8">
+		<title>Trippz - API Hackaton - Software AG</title>
+     <meta http-equiv="X-UA-Compatible" content="IE=8"></meta> 
+		<meta name="generator" content="Bootply" />
+		<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+		<link href="assets/css/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" type="text/css" href="//fonts.googleapis.com/css?family=Open+Sans" />
+     
+		<!--[if lt IE 9]>
+			<script src="//html5shim.googlecode.com/svn/trunk/html5.js"></script>
+		<![endif]-->
+		<link href="assets/css/styles.css" rel="stylesheet">
+	</head>
+	<body>
+
+<nav class="navbar navbar-fixed-top" role="navigation">
+        <div class="container">
+            <!-- Brand and toggle get grouped for better mobile display -->
+            <div class="navbar-header">
+                <a class="navbar-brand" href="#"><img src="./assets/img/logo.png"></a>                
+            </div>
+            <!-- /.navbar-collapse -->
+        </div>
+        <!-- /.container -->
+    </nav>
+
+
+<div class="row">
+  <div class="container" id="main">
+    <div class="row">
+    	<div class="col-xs-12">
+      
+        <h2 class="page-title">Find Buses, Train, Restaurants and Direction</h2>
+        
+        <!-- item list -->
+        <div class="box-contianer col-xs-4 left-box-contianer">
+         <form id='chennai_api_form'>
+            <div class="form-group">
+              <label for="email">Source:</label>
+              <input type="text" class="form-control" id="source" value="taramani">
+            </div>
+            <div class="form-group">
+              <label for="pwd">Destination:</label>
+              <input type="text" class="form-control" id="destination" value="koyambedu">
+            </div>
+            <button type="submit" class="btn btn-success" id='chennai_api_route'>Search</button>
+          </form>
+        </div>
+
+         <div class="display-info-right col-xs-8" style="display: none">
+            <div class="loader-div" id="loader" style="display: none"><img src="./assets/img/loader.gif"/></div>
+            <div class="info-div" id="loader" style="display: none">
+              <div class="row-info">
+                <div class="col-xs-3 nopadding left-label-form">Bus Route No.: </div>
+                <div class="col-xs-9 nopadding" id="bus_route"></div>
+              </div>
+
+              <div class="row-info">
+                <div class="col-xs-3 nopadding left-label-form">Local Train Time & Type: </div>
+                <div class="col-xs-9 nopadding"><div id="train_time_route"></div></div>
+              </div>
+
+              <div class="row-info">
+                    <div class="panel panel-default panel-restaurants">
+                      <div class="panel-heading">Nearby Destination Restaurants</div>
+                      <div class="panel-body restaurants-body">
+                        
+                      </div>
+                  </div>
+              </div>
+
+               <div class="row-info">
+                    <div class="panel panel-default panel-flights">
+                      <div class="panel-heading">Map</div>
+                      <iframe width="600" height="450" id="google-map" frameborder="0" style="border:0" allowfullscreen></iframe>
+                      </div>
+                  </div>
+              <div class="row-info">
+                    <div class="panel panel-default panel-flights">
+                      <div class="panel-heading">Fights</div>
+                      <div class="panel-body flights-body">                        
+                      </div>
+                  </div>
+              </div>
+              </div>
+
+            </div>
+          </div>
+      </div>
+    
+
+	<!-- script references -->
+		<script src="//ajax.googleapis.com/ajax/libs/jquery/2.0.2/jquery.min.js"></script>
+		<script src="assets/js/bootstrap.min.js" type="text/javascript"></script>
+		
+		<script src="assets/js/scripts.js" type="text/javascript"></script>
+	</body>
+</html>
